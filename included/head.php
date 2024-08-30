6158d58809f3d9277a61d1cb7fb3ab9855efb770
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-30 21:01:39 +0100

    "Added Open Graph metadata to head.php"

--- included/head.php
+++ included/head.php
@@ -6,6 +6,22 @@
 	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png" />
 	<link rel="icon" type="image/png" href="assets/img/favicon.png" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta property="og:title" content="<?= PAGE_TITLE ?>" />
+
+	<!-- Short description of the page -->
+	<meta property="og:description" content="A brief description of your website content." />
+
+	<!-- URL of the page -->
+	<meta property="og:url" content="https://okanlawon4316.rsucms24.com.ng/" />
+
+	<!-- Image URL to be used in the preview -->
+	<meta property="og:image" content="https://okanlawon4316.rsucms24.com.ng/assets/img/favicon.png" />
+
+	<!-- Type of the content (e.g., article, website, video) -->
+	<meta property="og:type" content="website" />
+
+	<!-- Site name (optional) -->
+	<meta property="og:site_name" content="Digital Logbook System" />
 	<title><?= PAGE_TITLE ?></title>
 	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" name="viewport" />
 	<!--     Fonts and icons     -->
