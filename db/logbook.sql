a3de368b6391fe9c0c71e54c9270fdaa1b1a4fc5
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-30 19:31:10 +0100

    "Updated database schema: added tables for departments and faculty, modified company table, removed data from feedbacks and logs tables, updated user data, and modified auto-increment values for various tables."

--- db/logbook.sql
+++ db/logbook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 24, 2024 at 11:07 AM
+-- Generation Time: Aug 30, 2024 at 08:28 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,20 +40,58 @@ CREATE TABLE `attachment` (
 
 CREATE TABLE `company` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `datetime` timestamp NOT NULL DEFAULT current_timestamp()
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`id`, `name`, `address`, `datetime`) VALUES
-(1, 'Harvoxx Tech Hub', 'Elzazi Plaza', '2024-06-18 19:16:02'),
-(2, 'Whyte Creativity', 'Elzazi Plaza', '2024-06-18 19:16:03'),
-(3, 'RSU', 'Tazi Plaza', '2024-06-18 19:16:03'),
-(4, 'NLNG', 'Plaza', '2024-06-18 19:16:03');
+INSERT INTO `company` (`id`, `name`) VALUES
+(1, 'Harvoxx Tech Hub'),
+(3, 'Agip'),
+(4, 'Whyte Creative'),
+(5, 'Code Flow');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `faculty_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `faculty_id`, `name`) VALUES
+(11, 4, 'English'),
+(12, 5, 'Computer Science'),
+(13, 5, 'Mathematics');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `faculty`
+--
+
+INSERT INTO `faculty` (`id`, `name`) VALUES
+(4, 'Humanities'),
+(5, 'Sciences');
 
 -- --------------------------------------------------------
 
@@ -70,18 +108,6 @@ CREATE TABLE `feedbacks` (
   `feedback` text NOT NULL,
   `datetime` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `feedbacks`
---
-
-INSERT INTO `feedbacks` (`id`, `supervisor_id`, `student_id`, `log_id`, `company_id`, `feedback`, `datetime`) VALUES
-(9, 2, 3, 5, 1, 'Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! Srew Ya! ', '2024-06-21 10:11:58'),
-(13, 2, 4, 8, 1, 'trying', '2024-06-22 11:41:29'),
-(17, 2, 3, 9, 1, ' and at Child giggles academy Im the best and at Child giggles academy Im the best and at Child giggles academy Im the best and at Child giggles academy Im the best and at Child giggles academy Im the best and at Child giggles academy', '2024-06-22 11:46:04'),
-(18, 2, 4, 7, 1, 'Damn Bloody Damn', '2024-06-22 11:59:18'),
-(21, 2, 3, 13, 1, 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas ducimus facere expedita beatae maiores commodi odit sapiente natus cumque quod?', '2024-07-18 11:15:04'),
-(22, 2, 3, 4, 1, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi odio omnis natus aut ab, facere, mollitia cumque nihil repellendus a ipsam.', '2024-07-22 18:19:23');
 
 -- --------------------------------------------------------
 
@@ -101,7 +127,7 @@ CREATE TABLE `lecturers` (
 --
 
 INSERT INTO `lecturers` (`id`, `user_id`, `faculty`, `department`) VALUES
-(2, 16, 'Science', 'Computer Science');
+(2, 2, '5', '12');
 
 -- --------------------------------------------------------
 
@@ -114,21 +140,9 @@ CREATE TABLE `logs` (
   `user_id` int(11) NOT NULL,
   `company_id` int(11) NOT NULL,
   `activity` text NOT NULL,
+  `attachment` text DEFAULT NULL,
   `datetime` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `logs`
---
-
-INSERT INTO `logs` (`id`, `user_id`, `company_id`, `activity`, `datetime`) VALUES
-(4, 13, 1, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi odio omnis natus aut ab, facere, mollitia cumque nihil repellendus a ipsam. Modi voluptatibus quia facere incidunt nihil soluta dolor veritatis quod officia unde ipsum eum blanditiis, facilis fugiat repudiandae et quibusdam dicta, mollitia ut doloribus optio sunt earum eveniet. Officiis?\r\n098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 ', '2024-06-19 10:30:19'),
-(5, 13, 1, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi odio omnis natus aut ab, facere, mollitia cumque nihil repellendus a ipsam. Modi voluptatibus quia facere incidunt nihil soluta dolor veritatis quod officia unde ipsum eum blanditiis, facilis fugiat repudiandae et quibusdam dicta, mollitia ut doloribus optio sunt earum eveniet. Officiis?\r\n098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 098765321 qwertyuiop 1234567890 asfghjkl 1234567890 mnbvcxz 0987654321 ', '2024-06-20 00:33:16'),
-(7, 15, 1, 'What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell What the hell ', '2024-06-19 20:14:58'),
-(8, 15, 1, 'Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 Part 2 ', '2024-06-20 20:15:58'),
-(10, 15, 1, 'I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC I lost my PC ', '2024-06-21 10:14:01'),
-(12, 15, 1, 'Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit Bloody Hell, Shit ', '2024-06-22 11:22:36'),
-(13, 13, 1, '12345678 12345678 12345678 12345678 12345678 12345678 12345678 12345678 12345678 12345678 12345678 12345678 12345678 ', '2024-06-24 12:31:31');
 
 -- --------------------------------------------------------
 
@@ -150,8 +164,7 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `user_id`, `matric`, `faculty`, `department`, `company_id`) VALUES
-(3, 13, 'DE:2020/4316', 'Science', 'Computer Science', 1),
-(4, 15, 'DE.2020/4276', 'Science', 'Computer Science', 1);
+(1, 4, 'DE.2020/9999', '5', '12', 1);
 
 -- --------------------------------------------------------
 
@@ -170,7 +183,7 @@ CREATE TABLE `supervisors` (
 --
 
 INSERT INTO `supervisors` (`id`, `user_id`, `company_id`) VALUES
-(2, 14, 1);
+(1, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -194,11 +207,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `phone`, `password`, `role`, `loginkey`) VALUES
-(2, 'Prime', 'Okanlawon', 'oyedelenewton@gmail.com', '09114895572', '$2y$10$jOOzpVYz2YxtUydtXjNZquvU/.qKbqAyPAgSV5ujYmhTHSMMLsx4.', 'admin', '$2y$10$88pTPtwU34J4MoPhzDOHz.ghM1/IOQBPPJsLHg3Yxr2EzrbSPNh.u'),
-(13, 'Martins', 'Okanlawon', 'martinsobomate@gmail.com', '08087837190', '$2y$10$bk.AusE5i.TKk4haJFzgjuRv8of/dP5ufkxBYAyXaxrklPbj0bKmS', 'student', '$2y$10$TohHXJFrXgFDdbBpv76uTOfwkMrNIQAqBlf/Zflhyv2kpdmbKHD/O'),
-(14, 'Super', 'Visor', 'super@visor.com', '1234567890', '$2y$10$mHhAU1MrP1b3sZWR2XWrR.iXe4eBE4fIMqUbCMs0yOQcQ4bG.b7ju', 'supervisor', '$2y$10$PhXI2jfFST.PhPVeU6d4yu0x0F49Wxxc2etuCco4DWMf6XeGLgw7C'),
-(15, 'Stud', 'ent', 'stud@ent.com', '98765432', '$2y$10$T26hqjBvk6edqut2eHkEW.djACWz1nD1j4NyePJugMC0LU4/y2Ivy', 'student', '$2y$10$wUhFNGskYHD6NYTXNt2LK.ZIXh3AaaSJ4Y0uehhrV4aIzyV34UVx6'),
-(16, 'Lectur', 'Er', 'lectur@er.com', '9876523457', '$2y$10$1SIpPJFJzwgO/wbgenUVreivMkQXzDkwl6dwd/KQxAXGmhZL1./1y', 'lecturer', '$2y$10$zmqDaAZoN4AK7SImm35B.OgjQJ9a0HC5MUxsJg7puw3xY74nmkPLi');
+(1, 'Admin', 'Admin', 'admin@gmail.com', '0123456789', '$2y$10$wKqJFwXSQsFIpoIdNQocRuWHoJfPB2JJzXaTXHopNvMzXCDPgtYwa', 'admin', '$2y$10$P0SkqBXufDt7GZkfpf.YXuMdBi2qd7Y1H4yxg.aKm0FSQTXDq2nCG'),
+(2, 'Lecturer', 'Lecturer', 'lecturer@gmail.com', '1234567890', '$2y$10$3cw46v6AKJAj6nPED0lc8uTSNvwtLbwxHlLdkouu0LFt52g.1dfpu', 'lecturer', '$2y$10$tiRHeACg4kp2GvK.kXTAtOvTUNsvvlrTj4PxXtajWwwsFbDckdBAS'),
+(3, 'Supervisor', 'Supervisor', 'supervisor@gmail.com', '2345678901', '$2y$10$t/cZKRrmSccqppmE7.b3Bu2QTp1WBmRZPx2VsQ1I3SpLQsA/0zfPa', 'supervisor', '$2y$10$VGKRDvpssD7gRFzl/s7b0.Pzz0j.g1Nov/2hmexlIS/kYPB9/gG36'),
+(4, 'Student1', 'Student1', 'student1@gmail.com', '3456789012', '$2y$10$B3WXSqj4PGP0VSO.6xWFge2X9zV0.MnM7Dbef3MiIn06zzHqAIaEq', 'student', '$2y$10$Jype3Jlb1oSAeQHD/W4nq.qnNa8LtdhhJzvgPk77X4fYFzXWypurO');
 
 --
 -- Indexes for dumped tables
@@ -208,6 +220,18 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `phone`, `password`
 -- Indexes for table `company`
 --
 ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -254,13 +278,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `feedbacks`
 --
 ALTER TABLE `feedbacks`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `lecturers`
@@ -272,25 +308,25 @@ ALTER TABLE `lecturers`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `supervisors`
 --
 ALTER TABLE `supervisors`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
